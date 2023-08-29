0f0d4b5c0d4d8855db9e37b1546581c5ba639a80
Anyanka Chibuzo Samson <senetor.samczar@gmail.com>
2023-08-29 22:30:09 +1200

    Updates

---
+++ precache-manifest.44d76939f4888d9ad1ff31c26114dc57.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fb3f5c6924e20a290f7d4dddea8407ea",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "e55e6204d82ee737ae05",
+    "url": "/portfolio/static/css/main.f6184861.chunk.css"
+  },
+  {
+    "revision": "050ee6d66f6c315d8b1b",
+    "url": "/portfolio/static/js/2.30df30a0.chunk.js"
+  },
+  {
+    "revision": "b952d6d32f30bab0684e61579c5d99b7",
+    "url": "/portfolio/static/js/2.30df30a0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e55e6204d82ee737ae05",
+    "url": "/portfolio/static/js/main.8eb5ce27.chunk.js"
+  },
+  {
+    "revision": "846826baf6a971be9100",
+    "url": "/portfolio/static/js/runtime-main.7f7ef08f.js"
+  },
+  {
+    "revision": "46bfa66adac6e2105a642ed40c492f59",
+    "url": "/portfolio/static/media/svg-1.46bfa66a.svg"
+  },
+  {
+    "revision": "f7bec5a1b22da81cb8f0d0d773966461",
+    "url": "/portfolio/static/media/svg-2.f7bec5a1.svg"
+  },
+  {
+    "revision": "e1de3bbdb22c47934da860ecfaeb3cce",
+    "url": "/portfolio/static/media/svg-backend.e1de3bbd.svg"
+  },
+  {
+    "revision": "61e69671f5a2beb23812ff6f5c99ea31",
+    "url": "/portfolio/static/media/svg-mobile.61e69671.svg"
+  },
+  {
+    "revision": "a73203bb7825e7a37d10091085dd9105",
+    "url": "/portfolio/static/media/svg-progress.a73203bb.svg"
+  },
+  {
+    "revision": "7010164117b02810e8dc6d4b9f8aecda",
+    "url": "/portfolio/static/media/video.70101641.mp4"
+  }
+]);
